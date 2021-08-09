1b4ec8a45af25f870c786e4d2b80333df17dc7b9
Vangalolu Yella Reddy <77666277+vyellareddy@users.noreply.github.com>
2021-08-09 14:29:00 +0530

    Add files via upload
    ClickTest.cs Contains the Code for Sendings Keys Example Alt Tab and Mouse Click Event Automatically for Every 5 Seconds

---
+++ ClickTest.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Click : Form
+    {
+        public class MouseOperations
+        {
+            [Flags]
+            public enum MouseEventFlags
+            {
+                LeftDown = 0x00000002,
+                LeftUp = 0x00000004,
+                MiddleDown = 0x00000020,
+                MiddleUp = 0x00000040,
+                Move = 0x00000001,
+                Absolute = 0x00008000,
+                RightDown = 0x00000008,
+                RightUp = 0x00000010
+            }
+
+            [DllImport("user32.dll", EntryPoint = "SetCursorPos")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool SetCursorPos(int X, int Y);
+
+            [DllImport("user32.dll")]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            private static extern bool GetCursorPos(out MousePoint lpMousePoint);
+
+            [DllImport("user32.dll")]
+            private static extern void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo);
+
+            public static void SetCursorPosition(int X, int Y)
+            {
+                SetCursorPos(X, Y);
+            }
+
+            public static MousePoint GetCursorPosition()
+            {
+                MousePoint currentMousePoint;
+                var gotPoint = GetCursorPos(out currentMousePoint);
+                if (!gotPoint) { currentMousePoint = new MousePoint(0, 0); }
+                return currentMousePoint;
+            }
+
+            public static void MouseEvent(MouseEventFlags value)
+            {
+                MousePoint position = GetCursorPosition();
+
+                mouse_event
+                    ((int)value,
+                     position.X,
+                     position.Y,
+                     0,
+                     0)
+                    ;
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct MousePoint
+            {
+                public int X;
+                public int Y;
+
+                public MousePoint(int x, int y)
+                {
+                    X = x;
+                    Y = x;
+                }
+
+            }
+
+        }
+
+        public Click()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+            timer = new Timer { Interval = 5000 };
+            timer.Start();
+            timer.Tick += OnTick;
+        }
+        private Timer timer; //Declare the timer at class level
+        private void OnTick(object sender, EventArgs eventArgs)
+        {
+            timer.Stop();
+            testAction();
+            timer.Start();
+        }
+       
+
+        public void testAction()
+        {
+            SendKeys.Send("%{TAB}");
+            //MouseOperations.SetCursorPosition(853, 411);
+            //MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+            //MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+
+        }
+
+    }
+
+}
